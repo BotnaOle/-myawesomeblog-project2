4dc75552179cbd490a116e64f69be4cbad5a9d31
BotnaOle <alescamen@gmail.com>
2022-07-13 23:09:14 +0300

    Add post model

--- events/models.py
+++ events/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.db.models import Model
 
 # Create your models here.
+class Event(models.Model):
+	event_image = models.ImageField(upload_to='event_images/')
+	event_text = models.CharField(max_length=200)
